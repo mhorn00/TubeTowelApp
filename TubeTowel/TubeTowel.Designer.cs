a73f3159b8307a65ac84dbdc1aadcbb4fb00e020
Myles <mylesfhorn@gmail.com>
2022-02-10 15:05:20 -0600

    Add project files.

---
+++ TubeTowel/TubeTowel.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace TubeTowel
+{
+    partial class TubeTowel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TubeTowel));
+            this.TubeTowelLbl = new System.Windows.Forms.Label();
+            this.ChkInOutLbl = new System.Windows.Forms.Label();
+            this.member = new System.Windows.Forms.TextBox();
+            this.tubes = new System.Windows.Forms.TextBox();
+            this.towels = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.MemLbl = new System.Windows.Forms.Label();
+            this.TubeLbl = new System.Windows.Forms.Label();
+            this.TowelLbl = new System.Windows.Forms.Label();
+            this.returnChk = new System.Windows.Forms.CheckBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.tubesTLbl = new System.Windows.Forms.Label();
+            this.TotalTubelsCountLbl = new System.Windows.Forms.Label();
+            this.TotalTowelsCountLbl = new System.Windows.Forms.Label();
+            this.TowelsTLbl = new System.Windows.Forms.Label();
+            this.CloseOutBtn = new System.Windows.Forms.Button();
+            this.creditLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TubeTowelLbl
+            // 
+            resources.ApplyResources(this.TubeTowelLbl, "TubeTowelLbl");
+            this.TubeTowelLbl.Name = "TubeTowelLbl";
+            this.TubeTowelLbl.TabStop = true;
+            this.TubeTowelLbl.UseMnemonic = false;
+            // 
+            // ChkInOutLbl
+            // 
+            resources.ApplyResources(this.ChkInOutLbl, "ChkInOutLbl");
+            this.ChkInOutLbl.Name = "ChkInOutLbl";
+            this.ChkInOutLbl.TabStop = true;
+            this.ChkInOutLbl.UseMnemonic = false;
+            // 
+            // member
+            // 
+            resources.ApplyResources(this.member, "member");
+            this.member.Name = "member";
+            this.member.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            this.member.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textbox_KeyPress);
+            // 
+            // tubes
+            // 
+            resources.ApplyResources(this.tubes, "tubes");
+            this.tubes.Name = "tubes";
+            this.tubes.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            this.tubes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textbox_KeyPress);
+            // 
+            // towels
+            // 
+            resources.ApplyResources(this.towels, "towels");
+            this.towels.Name = "towels";
+            this.towels.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            this.towels.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textbox_KeyPress);
+            // 
+            // MemLbl
+            // 
+            resources.ApplyResources(this.MemLbl, "MemLbl");
+            this.MemLbl.Name = "MemLbl";
+            this.MemLbl.TabStop = true;
+            // 
+            // TubeLbl
+            // 
+            resources.ApplyResources(this.TubeLbl, "TubeLbl");
+            this.TubeLbl.Name = "TubeLbl";
+            this.TubeLbl.TabStop = true;
+            // 
+            // TowelLbl
+            // 
+            resources.ApplyResources(this.TowelLbl, "TowelLbl");
+            this.TowelLbl.Name = "TowelLbl";
+            this.TowelLbl.TabStop = true;
+            // 
+            // returnChk
+            // 
+            resources.ApplyResources(this.returnChk, "returnChk");
+            this.returnChk.Name = "returnChk";
+            this.returnChk.UseVisualStyleBackColor = true;
+            this.returnChk.CheckedChanged += new System.EventHandler(this.returnChk_CheckedChanged);
+            this.returnChk.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.returnChk_KeyPress);
+            // 
+            // submitBtn
+            // 
+            resources.ApplyResources(this.submitBtn, "submitBtn");
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Press);
+            this.submitBtn.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.submitBtn_KeyPress);
+            // 
+            // Output
+            // 
+            this.Output.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            resources.ApplyResources(this.Output, "Output");
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.TabStop = false;
+            this.Output.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Output_KeyPress);
+            // 
+            // tubesTLbl
+            // 
+            resources.ApplyResources(this.tubesTLbl, "tubesTLbl");
+            this.tubesTLbl.Name = "tubesTLbl";
+            // 
+            // TotalTubelsCountLbl
+            // 
+            resources.ApplyResources(this.TotalTubelsCountLbl, "TotalTubelsCountLbl");
+            this.TotalTubelsCountLbl.Name = "TotalTubelsCountLbl";
+            // 
+            // TotalTowelsCountLbl
+            // 
+            resources.ApplyResources(this.TotalTowelsCountLbl, "TotalTowelsCountLbl");
+            this.TotalTowelsCountLbl.Name = "TotalTowelsCountLbl";
+            // 
+            // TowelsTLbl
+            // 
+            resources.ApplyResources(this.TowelsTLbl, "TowelsTLbl");
+            this.TowelsTLbl.Name = "TowelsTLbl";
+            // 
+            // CloseOutBtn
+            // 
+            resources.ApplyResources(this.CloseOutBtn, "CloseOutBtn");
+            this.CloseOutBtn.Name = "CloseOutBtn";
+            this.CloseOutBtn.TabStop = false;
+            this.CloseOutBtn.UseVisualStyleBackColor = true;
+            this.CloseOutBtn.Click += new System.EventHandler(this.CloseOutBtn_Click);
+            // 
+            // creditLbl
+            // 
+            resources.ApplyResources(this.creditLbl, "creditLbl");
+            this.creditLbl.Name = "creditLbl";
+            // 
+            // TubeTowel
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.creditLbl);
+            this.Controls.Add(this.CloseOutBtn);
+            this.Controls.Add(this.TotalTowelsCountLbl);
+            this.Controls.Add(this.TowelsTLbl);
+            this.Controls.Add(this.TotalTubelsCountLbl);
+            this.Controls.Add(this.tubesTLbl);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.returnChk);
+            this.Controls.Add(this.TowelLbl);
+            this.Controls.Add(this.TubeLbl);
+            this.Controls.Add(this.MemLbl);
+            this.Controls.Add(this.towels);
+            this.Controls.Add(this.tubes);
+            this.Controls.Add(this.member);
+            this.Controls.Add(this.ChkInOutLbl);
+            this.Controls.Add(this.TubeTowelLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TubeTowel";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.TubeTowle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TubeTowelLbl;
+        private System.Windows.Forms.Label ChkInOutLbl;
+        private System.Windows.Forms.TextBox member;
+        private System.Windows.Forms.TextBox tubes;
+        private System.Windows.Forms.TextBox towels;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label MemLbl;
+        private System.Windows.Forms.Label TubeLbl;
+        private System.Windows.Forms.Label TowelLbl;
+        private System.Windows.Forms.CheckBox returnChk;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Label tubesTLbl;
+        private System.Windows.Forms.Label TotalTubelsCountLbl;
+        private System.Windows.Forms.Label TotalTowelsCountLbl;
+        private System.Windows.Forms.Label TowelsTLbl;
+        private System.Windows.Forms.Button CloseOutBtn;
+        private System.Windows.Forms.Label creditLbl;
+    }
+}
+
---
+++ TubeTowel/TubeTowel.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace TubeTowel
+{
+    public partial class TubeTowel : Form
+    {
+
+        private int TotalTubes = 0;
+        private int TotalTowels = 0;
+        private Dictionary<String, Tuple<int, int>> checkList;
+        private FileStream log;
+
+        public TubeTowel()
+        {
+            InitializeComponent();
+            Size = new Size(600, 500);
+            if (!File.Exists("db.json") || (File.Exists("db.json") && new FileInfo("db.json").Length==0)) {
+                File.Create("db.json").Close();
+                checkList = new Dictionary<string, Tuple<int, int>>();
+            } else {
+                checkList = JsonConvert.DeserializeObject<Dictionary<String, Tuple<int, int>>>(File.ReadAllText("db.json"));
+                foreach (var e in checkList) {
+                    TotalTubes += e.Value.Item1;
+                    TotalTowels += e.Value.Item2;
+                }
+                TotalTubelsCountLbl.Text = TotalTubes.ToString();
+                TotalTowelsCountLbl.Text = TotalTowels.ToString();
+            }
+        }
+
+        private void TubeTowle_Load(object sender, EventArgs e){
+        }
+
+        private void returnChk_CheckedChanged(object sender, EventArgs e) {
+            if (tubes.Text.Length == 0 || tubes.Text.Length == 0) return;
+            if (returnChk.Checked) {
+                submitBtn.Text = "Check In";
+            } else {
+                submitBtn.Text = "Check Out";
+            }
+        }
+
+        private void returnChk_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar==10 || e.KeyChar == 13) {
+                returnChk.Checked = !returnChk.Checked;
+            }
+        }
+
+        private void textbox_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar == 10 || e.KeyChar == 13) {
+                ((TextBox)sender).Parent.SelectNextControl(ActiveControl, true, false, true, true);
+                e.Handled = true;
+            }
+            if ((e.KeyChar > 31 && e.KeyChar < 48) || e.KeyChar > 57) {
+                e.Handled = true; //ignore if not a number
+            }
+        }
+
+        private void Output_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = true;
+        }
+
+        private void submitBtn_Press(object sender, EventArgs e) {
+            submitBtn_submit((Button)sender);
+        }
+
+        private void submitBtn_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar==10 || e.KeyChar==13) {
+                submitBtn_submit((Button)sender);
+            }
+        }
+
+        private void submitBtn_submit(Button sender) {
+            if (member.Text.Length > 0 && tubes.Text.Length == 0 && towels.Text.Length == 0) {
+                if (checkList.ContainsKey(member.Text)) {
+                    checkList.TryGetValue(member.Text, out Tuple<int, int> tt);
+                    MessageBox.Show(this, $"Member \"{member.Text}\" has {tt.Item1} tubes and {tt.Item2} towels", "Memeber Info", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                } else {
+                    MessageBox.Show(this, "Member has nothing checked out", "Member Info", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                }
+                return;
+            }
+            Regex r = new Regex("^[0-9]{1,6}$");
+            if (!r.IsMatch(member.Text)) {
+                MessageBox.Show(this, "Member number is an invaild number", "Number Format Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                member.Text = "";
+                sender.Parent.Controls.Find("member", false)[0].Select();
+                return;
+            } else if (!r.IsMatch(tubes.Text)) {
+                MessageBox.Show(this, "Tubes is an invaild number", "Number Format Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                tubes.Text = "";
+                sender.Parent.Controls.Find("tubes", false)[0].Select();
+                return;
+            } else if (!r.IsMatch(towels.Text)) {
+                MessageBox.Show(this, "Towels is an invaild number", "Number Format Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                towels.Text = "";
+                sender.Parent.Controls.Find("towels", false)[0].Select();
+                return;
+            }
+            int tb = Int32.Parse(tubes.Text);
+            int tw = Int32.Parse(towels.Text);
+            int ltb, ltw = 0;
+            String mem = member.Text;
+            if (!returnChk.Checked) {
+                if (checkList.ContainsKey(mem)) {
+                    checkList.TryGetValue(mem, out Tuple<int, int> tt);
+                    ltb = tt.Item1 + tb;
+                    ltw = tt.Item2 + tw;
+                    checkList[mem] = new Tuple<int, int>(tt.Item1 + tb, tt.Item2 + tw);
+                } else {
+                    checkList.Add(mem, new Tuple<int, int>(tb, tw));
+                    ltb = tb;
+                    ltw = tw;
+                }
+                TotalTubes += tb;
+                TotalTowels += tw;
+            } else {
+                if (checkList.ContainsKey(mem)) {
+                    Tuple<int, int> tt;
+                    checkList.TryGetValue(mem, out tt);
+                    ltb = tt.Item1 - tb;
+                    ltw = tt.Item2 - tw;
+                    checkList[mem] = new Tuple<int, int>(tt.Item1 - tb, tt.Item2 - tw);
+                    TotalTubes -= tb;
+                    TotalTowels -= tw;
+                } else {
+                    MessageBox.Show(this, "Member has nothing checked out", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    return;
+                }
+            }
+            File.WriteAllText("db.json", JsonConvert.SerializeObject(checkList, Formatting.Indented));
+            Output_update(mem, tb, tw, returnChk.Checked, ltb, ltw);
+            TotalTubelsCountLbl.Text = TotalTubes.ToString();
+            TotalTowelsCountLbl.Text = TotalTowels.ToString();
+            member.Text = "";
+            tubes.Text = "";
+            towels.Text = "";
+            returnChk.Checked = false;
+        }
+        private void Output_update(String mem, int tb, int tw, bool inout, int ltb, int ltw) {
+            String strout = "[" + DateTime.Now.ToString("h:mm:ss tt") + "] ";
+            if (inout) {
+                strout += "Checked in ";
+            } else {
+                strout += "Checked out ";
+            }
+            strout += tb + " tubes and " + tw + " towels to member " + mem + " (" + ltb + " tubes, " + ltw + " towels total)\n";
+            Output.AppendText(strout);
+            Output.ScrollToCaret();
+            Output.Refresh();
+            File.AppendAllText("log.txt", strout);
+        }
+
+        private void textbox_TextChanged(object sender, EventArgs e) {
+            if (member.Text.Length > 0 && tubes.Text.Length == 0 && towels.Text.Length == 0) {
+                submitBtn.Text = "Member Info";
+            } else {
+                if (returnChk.Checked) {
+                    submitBtn.Text = "Check In";
+                } else {
+                    submitBtn.Text = "Check Out";
+                }
+            }
+        }
+
+        private void CloseOutBtn_Click(object sender, EventArgs e) {
+            DialogResult result = MessageBox.Show(this, "Are you sure you want to close out for the day? This cannot be undone.", "Close out for the day", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (result == DialogResult.Yes) {
+                submitBtn.Enabled = false;
+                member.Enabled = false;
+                tubes.Enabled = false;
+                towels.Enabled = false;
+                returnChk.Enabled = false;
+                CloseOutBtn.Enabled = false;
+                Output.Text = "Remaining tubes and towels are as follows:\n";
+                foreach (var i in checkList) {
+                    Output.AppendText($"Member \"{i.Key}\": {i.Value.Item1} Tubes, {i.Value.Item2} Towels (${(i.Value.Item1*20)+(i.Value.Item2*10)})\n");
+                }
+                File.AppendAllText("log.txt", "=====================End of Day=====================\n" + Output.Text);
+                if (!Directory.Exists("dbs")) Directory.CreateDirectory("dbs");
+                if (!Directory.Exists("logs")) Directory.CreateDirectory("logs");
+                File.Move("db.json", $"dbs\\db-{DateTime.Now.ToString("MM-dd-yyyy_HH.mm.ss")}.json");
+                File.Move("log.txt", $"logs\\log-{DateTime.Now.ToString("MM-dd-yyyy_HH.mm.ss")}.txt");
+            }
+        }
+    }
+}
